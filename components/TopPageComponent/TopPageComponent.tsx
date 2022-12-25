6f6f31951ade4f559eeac3bdc21a38320359515b
vmeset <vmeset@gmail.com>
2022-12-25 12:38:37 +0300

    TopPage title block

--- components/TopPageComponent/TopPageComponent.tsx
+++ components/TopPageComponent/TopPageComponent.tsx
@@ -1,10 +1,16 @@
 import { TopPageComponentProps } from "./TopPageComponent.props";
 import styles from './TopPageComponent.module.css'
+import { Tag } from "../Tag/Tag";
+import { Htag } from "../Htag/Htag";
 
 export const TopPageComponent = ({firstCategory, products, page}: TopPageComponentProps): JSX.Element => {
     return (
-        <div>
-            {products && products.length}
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag="h1">{page.title}</Htag>
+                {products && <Tag color="grey" sizer="m">{products.length}</Tag>}
+                <span>Сортировка</span>
+            </div>
         </div>
     )
 }
