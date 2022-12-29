6cc8e7fbc80e3adbf5ad5e556485912df51a782f
vmeset <vmeset@gmail.com>
2022-12-29 12:42:08 +0300

    declOfNum function for review[S]

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -6,7 +6,7 @@ import { Card } from "../Card/Card"
 import { Rating } from "../Rating/Rating"
 import { Tag } from "../Tag/Tag"
 import { Button } from "../Button/Button"
-import { priceRu } from "../../helpers/helpers"
+import { declOfNum, priceRu } from "../../helpers/helpers"
 import { Divider } from "../Divider/Divider"
 
 export const Product: FC<ProductProps> = ({product, className, ...props}): JSX.Element => {
@@ -33,7 +33,9 @@ export const Product: FC<ProductProps> = ({product, className, ...props}): JSX.E
             </div>
             <div className={styles.priceTitle}>цена</div>
             <div className={styles.creditTitle}>кредит</div>
-            <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+            <div className={styles.rateTitle}>
+                {product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+            </div>
             <Divider className={styles.hr} />
             <div className={styles.description}>{product.description}</div>
             <div className={styles.feature}>fi4i</div>
--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -12,4 +12,9 @@ export const firstLevelMenu: IFirstLevelMenu[] = [
     {route: 'products', name: 'Товары', icon: <BlockIcon />, id: ETopLevelCategory.Products}
 ]
 
-export const priceRu = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
+export const priceRu = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽')
+
+export const declOfNum = (number: number, titles: [string, string, string]): string => {
+	const cases = [2, 0, 1, 1, 1, 2]
+	return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]]
+}
